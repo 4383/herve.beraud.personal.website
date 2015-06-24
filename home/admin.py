2ff0acb5830f61512f4ba6c4d4e8db7abb55af4e
herve beraud <herve.beraud@openmailbox.org>
2015-06-24 23:33:43 +0200

    Framework is now an new object linked with technology

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from home.models import Technology
 from home.models import Category
+from home.models import Framework
+from home.models import Version
 
 
 class TechhnologyAdmin(admin.ModelAdmin):
@@ -11,5 +13,15 @@ class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
+class VersionAdmin(admin.ModelAdmin):
+    pass
+
+
+class FrameworkAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Technology, TechhnologyAdmin)
+admin.site.register(Version, VersionAdmin)
+admin.site.register(Framework, FrameworkAdmin)
--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
-    alt_description = models.CharField(max_length=150, default="")
+    alt_description = models.CharField(max_length=150, default=_("Show more details about this category"))
     priority_display = models.IntegerField(default=0)
     active_at_loading = models.BooleanField(default=False)
 
@@ -12,20 +13,44 @@ class Category(models.Model):
         return self.name
 
 
-class Technology(models.Model):
+class Version(models.Model):
     name = models.CharField(max_length=60, unique=True)
-    description = models.CharField(max_length=80)
-    version = models.CharField(max_length=60)
+    technical_name = models.CharField(max_length=60)
+    official_website = models.URLField(blank=True)
+    contextual_description = models.CharField(max_length=50, default=_("Show more details about this version"))
+    description = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.name + " " + self.technical_name
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.CharField(max_length=500)
+    contextual_description = models.CharField(max_length=50, default=_("Show more details about this technology"))
+    version = models.ManyToManyField(Version, blank=True)
+    official_website = models.URLField(blank=True, default="")
     level = models.IntegerField(default=0)
     category = models.ForeignKey(Category)
     usage = models.CharField(max_length=500)
     priority_display = models.IntegerField(default=0)
     tools = models.TextField(max_length=500, default="")
-    api = models.TextField(max_length=500, default="")
     icon = models.ImageField(default="", upload_to="technology", blank=True)
 
     def __str__(self):
         return self.name
 
-    def skills_level(self):
+    def convert_level_in_percent(self):
         return self.level * 100 / 5
+
+
+class Framework(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.CharField(max_length=500)
+    version = models.ManyToManyField(Version, blank=True)
+    technology = models.ForeignKey(Technology)
+    contextual_description = models.CharField(max_length=50, default=_("Visit the official website"))
+    official_website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
